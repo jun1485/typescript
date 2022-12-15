f06eade946c7e700f704b33841cb75aa52130280
주정준 <65483521+jun1485@users.noreply.github.com>
2022-12-15 09:22:42 +0900

    Update primitiveType.ts

--- primitiveType.ts
+++ primitiveType.ts
@@ -4,3 +4,7 @@ let isLoggedIn: boolean = false;
 
 let arr: number[] = [1,2,3];
 let arr: Array<number> = [1,2,3];
+
+let arr: [string, number] = ['hi', 10]; // tuple
+arr[1].concat('!'); // Error, 'number' does not have 'concat'
+arr[5] = 'hello'; // Error, Property '5' does not exist on type '[string, number]'.
