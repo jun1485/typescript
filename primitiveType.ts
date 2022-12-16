886be7a333a5fa05fe5e02d4b4f8bf62a0a9d7e9
주정준 <65483521+jun1485@users.noreply.github.com>
2022-12-16 09:26:14 +0900

    Update primitiveType.ts

--- primitiveType.ts
+++ primitiveType.ts
@@ -5,6 +5,9 @@ let isLoggedIn: boolean = false;
 let arr: number[] = [1,2,3];
 let arr: Array<number> = [1,2,3];
 
-let arr: [string, number] = ['hi', 10]; // tuple
+let arr: [string, number] = ['hi', 10];   // tuple
 arr[1].concat('!'); // Error, 'number' does not have 'concat'
 arr[5] = 'hello'; // Error, Property '5' does not exist on type '[string, number]'.
+
+enum Avengers { Capt, IronMan, Thor }
+let hero: Avengers = Avengers.Capt;     // enum
