4ff34f5228ca22d6d59b0e437e4e110fd806e756
주정준 <65483521+jun1485@users.noreply.github.com>
2022-12-27 23:08:53 +0900

    Update function.ts

--- function.ts
+++ function.ts
@@ -2,6 +2,9 @@ function sum(a, b) {    // normal JS
   return a + b;
 }
 
-function sum(a: number, b: number): number {    // TS
+function sum(a: number, b: number): number {
   return a + b;
 }
+sum(10, 20); // 30
+sum(10, 20, 30); // error, too many parameters
+sum(10); // error, too few parameters
