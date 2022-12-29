788f39ffcb7463268584ed04176e5e999bed7362
주정준 <65483521+jun1485@users.noreply.github.com>
2022-12-29 09:13:25 +0900

    Update function.ts

--- function.ts
+++ function.ts
@@ -8,3 +8,10 @@ function sum(a: number, b: number): number {
 sum(10, 20); // 30
 sum(10, 20, 30); // error, too many parameters
 sum(10); // error, too few parameters
+
+function sum(a: number, b?: number): number {     // ?연산
+  return a + b;
+}
+sum(10, 20); // 30
+sum(10, 20, 30); // error, too many parameters
+sum(10); // 타입 에러 없음
