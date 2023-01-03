b3add229be6f4a09ce139b6782837dd59455ebc6
주정준 <65483521+jun1485@users.noreply.github.com>
2023-01-03 09:28:49 +0900

    Update function.ts

--- function.ts
+++ function.ts
@@ -31,3 +31,10 @@ interface UIElement {
   // 아래 함수의 `this: void` 코드는 함수에 `this` 타입을 선언할 필요가 없다는 의미
   addClickListener(onclick: (this: void, e: Event) => void): void;
 }
+
+class Handler {
+    info: string;
+    onClick(this: Handler, e: Event) {
+        // 위의 `UIElement` 인터페이스의 스펙에 `this`가 필요없다고 했지만 사용했기 때문에 에러가 발생합니다.
+        this.info = e.message;
+    }
