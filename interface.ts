ebf1621cc3345e8f9f5c9eb61560ae93963f7a4f
주정준 <65483521+jun1485@users.noreply.github.com>
2023-01-08 18:45:12 +0900

    Update interface.ts

--- interface.ts
+++ interface.ts
@@ -4,3 +4,14 @@ function logAge(obj: { age: number }) {
   console.log(obj.age); // 28
 }
 logAge(person); // 28
+
+
+interface personAge {
+  age: number;
+}
+
+function logAge(obj: personAge) {
+  console.log(obj.age);
+}
+let person = { name: 'Capt', age: 28 };
+logAge(person);
