47ef331ad41b51c1d3187831faf2dd7b3abcdef2
주정준 <65483521+jun1485@users.noreply.github.com>
2023-01-11 09:15:59 +0900

    Update interface.ts

--- interface.ts
+++ interface.ts
@@ -29,3 +29,8 @@ function brewBeer(beer: CraftBeer) {
   console.log(beer.name); // Saporo
 }
 brewBeer(myBeer);
+
+// 정의되지 않은 속성이 사용되면 오류를 나타내준다.
+function brewBeer(beer: CraftBeer) {
+  console.log(beer.brewery); // Error: Property 'brewery' does not exist on type 'Beer'
+}
