13d226e5af418787ab9cc497a21bb3cfb3f31a8e
주정준 <65483521+jun1485@users.noreply.github.com>
2023-07-14 09:11:50 +0900

    Update generics.ts

--- generics.ts
+++ generics.ts
@@ -1,5 +1,13 @@
 // 제네릭 사용 시 컴포넌트가 다양한 타입에 대해 작동 가능
 // 컴파일 시간에 타입의 정보를 유지.
 
-let numbers: Array<number> = [1, 2, 3];
-let strings: Array<string> = ['Hello', 'World'];
+let numbers: Array<number> = [1, 2, 3];  // 숫자 배열
+let strings: Array<string> = ['Hello', 'World'];  // 문자열 배열
+
+
+function identity<T>(arg: T): T {  // 매개변수 타입 T
+  return arg;
+}
+
+let output1 = identity<string>('Hello');
+let output2 = identity<number>(123);
