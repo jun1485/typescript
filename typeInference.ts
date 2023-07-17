b19001315b509d5c3ed39e004d1cd736e1919daf
주정준 <65483521+jun1485@users.noreply.github.com>
2023-07-17 09:12:11 +0900

    Update typeInference.ts

--- typeInference.ts
+++ typeInference.ts
@@ -3,3 +3,11 @@
 
 let name = 'Jun';  // 타입 추론에 의해 name은 string 타입으로 추론됨
 let age = 26;       // 타입 추론에 의해 age는 number 타입으로 추론됨
+
+
+// 함수 반환 값으로 타입 추론
+function multiply(a: number, b: number) {
+  return a * b;     // 반환 값으로 인해 multiply 함수의 타입이 number로 추론됨
+}
+
+let result = multiply(5, 10);  // result는 number 타입으로 추론됨
