ae70645551d152c3371fcb7f314c602adc476b59
주정준 <65483521+jun1485@users.noreply.github.com>
2023-07-28 09:44:13 +0900

    Update indexType.ts

--- indexType.ts
+++ indexType.ts
@@ -2,6 +2,7 @@
 // 타입 시스템을 통해 정적으로 확인할 수 없는 속성에도 타입 안정성을 부여.
 
 // 문자열 인덱스 타입(String Index Types)
+// 객체의 속성과 값을 매핑, 속성 이름을 동적으로 사용 가능.
 interface Person {
   name: string;
   age: number;
@@ -14,3 +15,7 @@ const person: Person = {
   city: 'New York',
   country: 'USA',
 };
+
+
+// 숫자 인덱스 타입(Number Index Types)
+// 배열이나 객체의 요소 타입 정의 가능.
