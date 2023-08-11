096671ff160ed9ba93493ff61cf0536bd5bca18a
주정준 <65483521+jun1485@users.noreply.github.com>
2023-08-11 09:13:23 +0900

    Update partial.ts

--- partial.ts
+++ partial.ts
@@ -8,3 +8,13 @@ interface Person {
 
 // Partial을 사용하여 모든 속성을 선택적으로 만듭니다.
 const partialPerson: Partial<Person> = { name: 'Alice' };
+
+
+// Partial의 내부 정의
+type Partial<T> = {
+  [P in keyof T]?: T[P];
+};
+//   keyof T: 객체 T의 모든 키를 추출합니다.
+//   [P in keyof T]: T의 모든 키를 순회하면서
+//   T[P]: 각 키에 해당하는 타입을 참조하고
+//   ?:: 선택적으로 만듭니다.
