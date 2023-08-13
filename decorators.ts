a30ae8af29ec74fb6809cb25e1afb9f15ec27043
주정준 <65483521+jun1485@users.noreply.github.com>
2023-08-13 23:27:33 +0900

    Create decorators.ts

---
+++ decorators.ts
@@ -0,0 +1,9 @@
+// Decorators는 TS에서 클래스, 메서드, 접근자, 프로퍼티, 매개변수 등을 수정하거나 추가 기능을 부여하는 기능.
+
+// Decorators 사용 전 'tsconfig.json'에서 다음 옵션을 설정.
+{
+  "compilerOptions": {
+    "experimentalDecorators": true,
+    "target": "ES5" // 또는 그 이상
+  }
+}
