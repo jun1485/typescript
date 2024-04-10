28805899142d08f54b8babb4e356c59669877603
주정준 <65483521+jun1485@users.noreply.github.com>
2024-04-10 20:41:42 +0900

    Update partial.ts

--- partial.ts
+++ partial.ts
@@ -1,20 +1,14 @@
-// Partial은 유틸리티 타입 중 하나로, 객체의 모든 속성을 선택적으로 만듦.
-// 즉, 해당 객체의 모든 속성이 있어도 되고 없어도 되는 상태가 됨.
+// Partial<T> 유틸리티 타입은 'T' 타입의 모든 속성을 선택적으로 만듦.
+// 'T'타입에 있는 모든 속성을 포함하되, 각 속성이 필수가 아닌 선택 사항으로 변경됨.
+// 기존 타입의 일부 속성만을 사용해야 할 때 유용하게 사용.
 
-interface Person {
+interface User {
+  id: number;
   name: string;
-  age: number;
+  email: string;
 }
 
-// Partial을 사용하여 모든 속성을 선택적으로 만듭니다.
-const partialPerson: Partial<Person> = { name: 'Alice' };
-
-
-// Partial의 내부 정의
-type Partial<T> = {
-  [P in keyof T]?: T[P];
+// User 인터페이스의 모든 속성을 선택적으로 만듭니다.
+const updateUser: Partial<User> = {
+  email: "example@example.com"
 };
-//   keyof T: 객체 T의 모든 키를 추출합니다.
-//   [P in keyof T]: T의 모든 키를 순회하면서
-//   T[P]: 각 키에 해당하는 타입을 참조하고
-//   ?:: 선택적으로 만듭니다.
